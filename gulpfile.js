6a5f7a8b4364e0ed6d82447ba6498b8ce571b69f
sampo virmasalo <sampo.virmasalo@gmail.com>
2016-11-23 13:18:11 +0000

    Sass bug fixed

--- gulpfile.js
+++ gulpfile.js
@@ -72,6 +72,7 @@ PREFIX. CONCAT AND MINIFY STYLES
 gulp.task('styles', () =>
     gulp.src('scss/*.scss')
         .pipe(sourcemaps.init())
+        .pipe(sass().on('error',sass.logError))
 		.pipe(autoprefixer('last 3 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')) // Adds browser prefixes (eg. -webkit, -moz, etc.)
         .pipe(concat('all.min.css'))
         .pipe(minifycss({compatibility: 'ie8'},{debug:true}, function(details) {
